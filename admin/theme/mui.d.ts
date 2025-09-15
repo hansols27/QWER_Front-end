c9c41b64ecb3a3a057fff3babdecf7697d9b8907
hansols27 <tpfmgks@naver.com>
2025-09-15 14:44:21 +0900

    modify

--- admin/theme/mui.d.ts
+++ admin/theme/mui.d.ts
@@ -1,38 +1,21 @@
 import '@mui/material/styles';
 import '@mui/lab/Timeline';
-import '@mui/lab/TimelineDot';
-import '@mui/lab/TimelineConnector';
-import '@mui/lab/TimelineContent';
+import '@mui/lab/TabPanel';
+import '@mui/material/Chip';
 
 declare module '@mui/material/styles' {
   interface Components {
     MuiTimeline?: {
-      styleOverrides?: {
-        root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
-      };
-      variants?: Array<{
-        props: any;
-        style: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
-      }>;
+      styleOverrides?: { root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties) };
+      variants?: Array<{ props: any; style: React.CSSProperties | ((ownerState: any) => React.CSSProperties) }>;
     };
     MuiTimelineDot?: {
-      styleOverrides?: {
-        root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
-      };
-      variants?: Array<{
-        props: any;
-        style: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
-      }>;
+      styleOverrides?: { root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties) };
+      variants?: Array<{ props: any; style: React.CSSProperties | ((ownerState: any) => React.CSSProperties) }>;
     };
-    MuiTimelineConnector?: {
-      styleOverrides?: {
-        root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
-      };
-    };
-    MuiTimelineContent?: {
-      styleOverrides?: {
-        root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
-      };
-    };
+    MuiTimelineConnector?: { styleOverrides?: { root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties) } };
+    MuiTimelineContent?: { styleOverrides?: { root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties) } };
+    MuiTabPanel?: { styleOverrides?: { root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties) } };
+    MuiChip?: { variants?: Array<{ props: any; style: React.CSSProperties | ((ownerState: any) => React.CSSProperties) }> };
   }
 }
