12961d9c327b88e4f2bc679258830e2be20ffd37
hansols27 <tpfmgks@naver.com>
2025-09-15 13:41:15 +0900

    modify

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias['@theme'] = path.resolve(__dirname, 'admin/theme');
     config.resolve.alias['@lib'] = path.resolve(__dirname, 'admin/lib');
+    config.resolve.alias['@frontend'] = path.resolve(__dirname, 'front_end/src');
+    config.resolve.alias['@shared'] = path.resolve(__dirname, 'shared');
     return config;
   },
 };
