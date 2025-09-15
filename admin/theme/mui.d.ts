c878629b864f317313e7090b7c6a6cd0da2a239b
hansols27 <tpfmgks@naver.com>
2025-09-15 14:38:37 +0900

    modify

--- admin/theme/mui.d.ts
+++ admin/theme/mui.d.ts
@@ -2,10 +2,16 @@ import '@mui/material/styles';
 
 declare module '@mui/material/styles' {
   interface Components {
+    MuiTimeline?: {
+      styleOverrides?: {
+        root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
+      };
+    };
     MuiTabPanel?: {
       styleOverrides?: {
         root?: React.CSSProperties | ((ownerState: any) => React.CSSProperties);
       };
     };
+    // 필요한 다른 컴포넌트도 여기에 추가 가능
   }
 }
