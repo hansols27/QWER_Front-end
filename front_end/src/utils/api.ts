7dfd74de3cac6c4855a9260a06007abd0f55b2ac
hansols27 <tpfmgks@naver.com>
2025-09-17 15:27:43 +0900

    modify

--- front_end/src/utils/api.ts
+++ front_end/src/utils/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
-  withCredentials: true,
+  baseURL: "/api", // Next.js API 라우트 기준
 });
 
 export default api;
