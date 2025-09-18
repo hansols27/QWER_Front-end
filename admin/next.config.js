af82eedac3aefbf4d78c9d96fb93eb5605f4e2c8
hansols27 <tpfmgks@naver.com>
2025-09-18 15:52:29 +0900

    modify

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,13 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 🔑 shared 폴더도 빌드 타겟으로 포함
+  transpilePackages: ["shared"],
+
   webpack: (config) => {
-    config.resolve.alias['@theme'] = path.resolve(__dirname, 'theme');       // admin/theme
-    config.resolve.alias['@lib'] = path.resolve(__dirname, 'lib');           // admin/lib
-    config.resolve.alias['@front'] = path.resolve(__dirname, '../front_end/src'); // front_end/src
-    config.resolve.alias['@shared'] = path.resolve(__dirname, '../shared');        // shared
-     config.resolve.alias['@api'] = path.resolve(__dirname, '../front_end/src/utils/api.ts'); // front_end/src/utils/api.ts
+    config.resolve.alias["@theme"] = path.resolve(__dirname, "theme"); // admin/theme
+    config.resolve.alias["@lib"] = path.resolve(__dirname, "lib"); // admin/lib
+    config.resolve.alias["@front"] = path.resolve(__dirname, "../front_end/src"); // front_end/src
+    config.resolve.alias["@shared"] = path.resolve(__dirname, "../shared"); // shared
+    config.resolve.alias["@api"] = path.resolve(__dirname, "../front_end/src/utils/api.ts"); // front_end/src/utils/api.ts
     return config;
   },
 };
