6401cfb60108ba77d3c06acdb06dda0df534f169
hansols27 <tpfmgks@naver.com>
2025-09-18 16:24:34 +0900

    modify

--- shared/services/settings.ts
+++ shared/services/settings.ts
@@ -1,5 +1,5 @@
 import { SettingsData } from "@shared/types/settings";
-import { get, postFormData } from "@api";
+import { get, postFormData } from "@shared/utils/api";
 
 // 설정 불러오기
 export const getSettings = async (): Promise<SettingsData> => {
