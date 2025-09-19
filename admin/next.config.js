f0b51b6a51d73777819b415b3fe3882690c681e2
hansols27 <tpfmgks@naver.com>
2025-09-19 15:30:11 +0900

    modify

--- admin/next.config.js
+++ admin/next.config.js
@@ -2,12 +2,41 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
+  // Next.js 워크스페이스 루트 명시 (복수 lockfile 문제 해결)
+  outputFileTracingRoot: path.join(__dirname, "../"),
+
   webpack: (config) => {
-    config.resolve.alias["@theme"] = path.resolve(__dirname, "theme");
-    config.resolve.alias["@lib"] = path.resolve(__dirname, "lib");
-    config.resolve.alias["@front"] = path.resolve(__dirname, "../front_end/src");
-    config.resolve.alias["@utils"] = path.resolve(__dirname, "../front_end/src/utils");
-    config.resolve.alias["@shared"] = path.resolve(__dirname, "../shared/");
+    // Alias 설정
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@theme": path.resolve(__dirname, "theme"),
+      "@lib": path.resolve(__dirname, "lib"),
+      "@front": path.resolve(__dirname, "../front_end/src"),
+      "@utils": path.resolve(__dirname, "../front_end/src/utils"),
+      "@shared": path.resolve(__dirname, "../shared"),
+    };
+
+    // 외부 TypeScript / TSX 파일 트랜스파일
+    const externalTSPaths = [
+      path.resolve(__dirname, "../front_end/src"),
+      path.resolve(__dirname, "../shared"),
+    ];
+
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      include: externalTSPaths,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["next/babel"], // Next.js 기본 preset
+          },
+        },
+      ],
+    });
+
     return config;
   },
 };
