2ca61fe32e0d537f1786fa593a3d5afa434040a4
hansols27 <tpfmgks@naver.com>
2025-09-25 13:20:23 +0900

    modify

--- back_end/dist/app.js
+++ back_end/dist/app.js
@@ -8,7 +8,10 @@ const cors_1 = __importDefault(require("cors"));
 const settings_1 = __importDefault(require("./routes/settings"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["https://your-admin.vercel.app", "https://your-front.vercel.app"]
+    origin: [
+        "https://qwer-fansite-admin.vercel.app", // admin 페이지의 실제 URL
+        "https://qwer-fansite-front.vercel.app" // front_end 페이지의 실제 URL 
+    ]
 }));
 app.use(express_1.default.json());
 app.use("/api/settings", settings_1.default);
